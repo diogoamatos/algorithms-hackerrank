3ffcc35f97eb1d3b6f73ef9d6606a239c1031385
Diogo Matos <alvesdiogo.mts@gmail.com>
2017-04-18 15:41:37 -0300

    Implementation 04 - Betwen two sets

---
+++ 02.Implementation/04.py
@@ -0,0 +1,39 @@
+#!/bin/python
+# Between two sets
+"""
+1. All elements in A are factors of x.
+2 x is a factor of all elements in B.
+"""
+
+
+n, m = raw_input().strip().split(' ')
+n, m = [int(n), int(m)]
+a = map(int, raw_input().strip().split(' '))
+b = map(int, raw_input().strip().split(' '))
+
+
+def mmc(x, vetor):
+    for valor in vetor:
+        if valor % x:
+            return False
+    return True
+
+
+def mmc_selector(x, vetor):
+    selected = []
+    for valor in vetor:
+        if valor >= x and valor % x == 0 and valor not in selected:
+            selected.append(valor)
+    return selected
+
+x = []
+
+for i in range(1, min(b)+1):
+    if mmc(i, b) and i not in x:
+        x.append(i)
+
+for i in a:
+    x = mmc_selector(i, x)
+
+print len(x)
+# print x_filtered
