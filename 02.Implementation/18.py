ce4e6500f9b824080178a596b9fee7a6f81693dd
Diogo Matos <alvesdiogo.mts@gmail.com>
2017-06-22 13:26:25 -0300

    ex 18 - climbing the leaderboard - timeout test 6,7,8 e 9

---
+++ 02.Implementation/18.py
@@ -0,0 +1,39 @@
+#!/bin/python
+# Climbing the leaderboard
+
+n = int(raw_input().strip())
+scores = map(int,raw_input().strip().split(' '))
+m = int(raw_input().strip())
+alice = map(int,raw_input().strip().split(' '))
+# your code goes here
+
+rank = []
+for i in scores:
+    if i not in rank:
+        rank.append(i)
+
+pos = len(rank)
+# aux = []
+
+for nota in alice:
+    tmp = 0
+    position = 0
+
+    for i in range(pos-1, -1, -1):
+        if nota <= rank[i]:
+            if nota == rank[i]:
+                # aux.append(i+1)
+                position = i+1
+            else:
+                # aux.append(i+2)
+                position = i+2
+            tmp = i+1
+            break
+
+        if nota > rank[i] and i == 0:
+            tmp = i+1
+            # aux.append(tmp)
+            position = i+1
+
+    pos = tmp
+    print position
